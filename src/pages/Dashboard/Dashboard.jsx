b46c2f863bf5264e9570451ec4a8bc5df5473ad6
Sammydepoj <samueladepoju86@yahoo.com>
2023-04-06 17:51:04 +0100

    updated: updated the content of the dashboard page structure

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,20 +2,7 @@ import React from "react";
 import AppLayout from "../../layout/AppLayout";
 import "./Dashboard.css";
 
-import {
-  Anchor,
-  Col,
-  Row,
-  Button,
-  Form,
-  Input,
-  Radio,
-  message,
-  Upload,
-} from "antd";
-import { UploadOutlined } from "@ant-design/icons";
-
-import { UploadProps } from "antd";
+import { Button, Form, Input, Radio } from "antd";
 
 import userImg from "./assets/userImg.png";
 
@@ -49,15 +36,139 @@ const Dashboard = () => {
     console.log(value);
   };
 
+  const { TextArea } = Input;
+
   return (
     <AppLayout>
       <div className="dashboard">
         <h3>Your Dashboard</h3>
-        {/* <div className="dashboardContainer">
-          <div className="sidebar"></div>
-          <div className="dahboardBody"></div>
-        </div> */}
-        <Row>
+        <div className="dashboardContainer">
+          <div className="sidebar">
+            <div className="userProfile">
+              <img src={userImg} alt="user profile" />
+              <p className="username">Titilayo</p>
+              <p className="userCareer">Career Speaker</p>
+            </div>
+            <ul className="userMenu">
+              <li>
+                <a href="#Part1">Personal Details</a>
+              </li>
+              <li>
+                <a href="#Part1">Niche</a>
+              </li>
+              <li>
+                <a href="#Part1">Availability & Fees</a>
+              </li>
+              <li>
+                <a href="#Part1">Account Preferences</a>
+              </li>
+              <li>
+                <a href="#Part1">Publications</a>
+              </li>
+              <li>
+                <a href="#Part1">Passwords</a>
+              </li>
+              <li>
+                <a href="#Part1">Email</a>
+              </li>
+              <li>
+                <a href="#Part1">Signature</a>
+              </li>
+              <li>
+                <a href="#Part1">Media</a>
+              </li>
+            </ul>
+          </div>
+          <div className="dahsboardBody">
+            <div className="formContainer">
+              <p className="dashboardDetail">
+                Spefind automatically saves your dashboard changes
+              </p>
+              <Form name="form_item_path" layout="vertical" onFinish={onFinish}>
+                <MyFormItemGroup prefix={["user"]}>
+                  <MyFormItemGroup prefix={["name"]}>
+                    <MyFormItem name="firstName" label="First Name">
+                      <Input aria-label="firstname" placeholder="Type here" />
+                    </MyFormItem>
+                    <MyFormItem name="lastName" label="Last Name">
+                      <Input aria-label="lastname" placeholder="Type here" />
+                    </MyFormItem>
+                  </MyFormItemGroup>
+
+                  <MyFormItem name="age" label="Age">
+                    <Input aria-label="age" placeholder="Type here" />
+                  </MyFormItem>
+
+                  <MyFormItem
+                    name="professional_credentials"
+                    label="Professional Credentials"
+                  >
+                    <Input
+                      aria-label="professional_credentials"
+                      placeholder="Type here"
+                    />
+                  </MyFormItem>
+
+                  <MyFormItem
+                    name="prefered_pronouns"
+                    label="Prefered pronouns"
+                  >
+                    <Input
+                      aria-label="prefered_pronouns"
+                      placeholder="Type here"
+                    />
+                  </MyFormItem>
+
+                  <MyFormItem name="gender" label="Gender">
+                    <Radio>Male</Radio>
+                    <Radio>Female</Radio>
+                    <Radio>Other</Radio>
+                  </MyFormItem>
+                  <MyFormItem name="coverBanner" label="Cover Banner">
+                    <Button className="selectFile">Select File</Button>
+                  </MyFormItem>
+
+                  <MyFormItem
+                    name="editProfilePicture"
+                    label="Edit Profile Picture"
+                  >
+                    <div className="editProfilePicture">
+                      <img src={userImg} alt="edit your profile avatar" />
+                      <div>
+                        <Button>Remove</Button>
+                        <Button>Change Picture</Button>
+                        <Button>Crop Picture</Button>
+                      </div>
+                    </div>
+                  </MyFormItem>
+
+                  <MyFormItem
+                    name="country_or_state"
+                    label="Country or State & City"
+                  >
+                    <Input
+                      aria-label="country_or_state"
+                      placeholder="Type here"
+                    />
+                  </MyFormItem>
+
+                  <MyFormItem name="timeZone" label="Time Zone">
+                    <Input aria-label="timeZone" placeholder="Type here" />
+                  </MyFormItem>
+
+                  <MyFormItem name="biography" label="Biography">
+                    <TextArea placeholder="Type here" />
+                  </MyFormItem>
+
+                  <MyFormItem name="whyChooseMe" label="Why Choose Me?">
+                    <TextArea placeholder="Type here" />
+                  </MyFormItem>
+                </MyFormItemGroup>
+              </Form>
+            </div>
+          </div>
+        </div>
+        {/* <Row>
           <Col span={8}>
             <div className="userProfile">
               <img src={userImg} alt="user profile" />
@@ -187,9 +298,7 @@ const Dashboard = () => {
               </div>
             </div>
 
-            {/* <form action="#">
-              <label htmlFor="firstName">First Name</label>
-            </form> */}
+    
 
             <div
               id="part-2"
@@ -206,7 +315,7 @@ const Dashboard = () => {
               }}
             />
           </Col>
-        </Row>
+        </Row> */}
       </div>
     </AppLayout>
   );
