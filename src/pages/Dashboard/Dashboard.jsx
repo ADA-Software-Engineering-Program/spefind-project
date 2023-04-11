8b2f8deda83d0e63e698a415e98b38546dd37397
Adepoju Adeyemi Samuel <samueladepoju86@yahoo.com>
2023-04-11 15:08:50 +0100

    dashbard page implementation completed

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,49 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import AppLayout from "../../layout/AppLayout";
 import "./Dashboard.css";
 
-import { Button, Form, Input, Radio } from "antd";
+import { Form, Input } from "antd";
 
 import userImg from "./assets/userImg.png";
 
 const Dashboard = () => {
-  const MyFormItemContext = React.createContext([]);
-
-  function toArr(str) {
-    return Array.isArray(str) ? str : [str];
-  }
-
-  const MyFormItemGroup = ({ prefix, children }) => {
-    const prefixPath = React.useContext(MyFormItemContext);
-    const concatPath = React.useMemo(
-      () => [...prefixPath, ...toArr(prefix)],
-      [prefixPath, prefix]
-    );
-    return (
-      <MyFormItemContext.Provider value={concatPath}>
-        {children}
-      </MyFormItemContext.Provider>
-    );
-  };
-  const MyFormItem = ({ name, ...props }) => {
-    const prefixPath = React.useContext(MyFormItemContext);
-    const concatName =
-      name !== undefined ? [...prefixPath, ...toArr(name)] : undefined;
-    return <Form.Item name={concatName} {...props} />;
-  };
+  const [isNavExpanded, setIsNavExpanded] = useState(true);
 
   const onFinish = (value) => {
     console.log(value);
   };
 
-  const { TextArea } = Input;
-
   return (
     <AppLayout>
       <div className="dashboard">
         <h3>Your Dashboard</h3>
+        <button
+          className="harmburger"
+          // setting the isNavExpanded variable to the opposite of whatever it is when users click on the button
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="black"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
         <div className="dashboardContainer">
-          <div className="sidebar">
+          <div className={isNavExpanded ? "sidebar" : "sidebarMobile"}>
             <div className="userProfile">
               <img src={userImg} alt="user profile" />
               <p className="username">Titilayo</p>
@@ -79,91 +74,131 @@ const Dashboard = () => {
               </li>
             </ul>
           </div>
+
           <div className="dahsboardBody">
             <div className="formContainer">
               <p className="dashboardDetail">
                 Spefind automatically saves your dashboard changes
               </p>
               <Form name="form_item_path" layout="vertical" onFinish={onFinish}>
-                <MyFormItemGroup prefix={["user"]}>
-                  <MyFormItemGroup prefix={["name"]}>
-                    <MyFormItem name="firstName" label="First Name">
-                      <Input aria-label="firstname" placeholder="Type here" />
-                    </MyFormItem>
-                    <MyFormItem name="lastName" label="Last Name">
-                      <Input aria-label="lastname" placeholder="Type here" />
-                    </MyFormItem>
-                  </MyFormItemGroup>
+                <label htmlFor="firstName"> First Name</label>
+                <Input
+                  aria-label="firstname"
+                  placeholder="Type here"
+                  id="firstName"
+                />
 
-                  <MyFormItem name="age" label="Age">
-                    <Input aria-label="age" placeholder="Type here" />
-                  </MyFormItem>
+                <label htmlFor="lastName"> Last Name</label>
+                <Input
+                  aria-label="lastname"
+                  placeholder="Type here"
+                  id="lastName"
+                />
 
-                  <MyFormItem
-                    name="professional_credentials"
-                    label="Professional Credentials"
-                  >
-                    <Input
-                      aria-label="professional_credentials"
-                      placeholder="Type here"
-                    />
-                  </MyFormItem>
+                <label htmlFor="age"> Age</label>
+                <Input aria-label="age" placeholder="Type here" id="age" />
 
-                  <MyFormItem
-                    name="prefered_pronouns"
-                    label="Prefered pronouns"
-                  >
-                    <Input
-                      aria-label="prefered_pronouns"
-                      placeholder="Type here"
-                    />
-                  </MyFormItem>
+                <label htmlFor="professional_credentials">
+                  Professional Credentials
+                </label>
+                <Input
+                  aria-label="professional_credentials"
+                  placeholder="Type here"
+                />
 
-                  <MyFormItem name="gender" label="Gender">
-                    <Radio>Male</Radio>
-                    <Radio>Female</Radio>
-                    <Radio>Other</Radio>
-                  </MyFormItem>
-                  <MyFormItem name="coverBanner" label="Cover Banner">
-                    <Button className="selectFile">Select File</Button>
-                  </MyFormItem>
+                <label htmlFor="prefered_pronouns">Prefered pronouns</label>
+                <Input
+                  aria-label="prefered_pronouns"
+                  placeholder="Type here"
+                  id="prefered_pronouns"
+                />
 
-                  <MyFormItem
-                    name="editProfilePicture"
-                    label="Edit Profile Picture"
-                  >
-                    <div className="editProfilePicture">
-                      <img src={userImg} alt="edit your profile avatar" />
-                      <div>
-                        <Button>Remove</Button>
-                        <Button>Change Picture</Button>
-                        <Button>Crop Picture</Button>
-                      </div>
-                    </div>
-                  </MyFormItem>
+                <label htmlFor="gender">Gender</label>
 
-                  <MyFormItem
-                    name="country_or_state"
-                    label="Country or State & City"
-                  >
-                    <Input
-                      aria-label="country_or_state"
-                      placeholder="Type here"
-                    />
-                  </MyFormItem>
+                <div className="genderWrapper">
+                  <input
+                    id="male"
+                    type="radio"
+                    aria-label="male"
+                    className="newInput"
+                    value={"male"}
+                    name={"gender"}
+                  />
+                  <label htmlFor="male">Male</label>
+                </div>
+                <div className="genderWrapper">
+                  <input
+                    id="female"
+                    type="radio"
+                    aria-label="female"
+                    className="newInput"
+                    value={"female"}
+                    name={"gender"}
+                  />
+                  <label htmlFor="female">Female</label>
+                </div>
+                <div className="genderWrapper">
+                  <input
+                    id="others"
+                    type="radio"
+                    aria-label="others"
+                    className="newInput"
+                    value={"others"}
+                    name={"gender"}
+                  />
+                  <label htmlFor="others">Others</label>
+                </div>
 
-                  <MyFormItem name="timeZone" label="Time Zone">
-                    <Input aria-label="timeZone" placeholder="Type here" />
-                  </MyFormItem>
+                <label htmlFor="coverBanner">
+                  Cover Banner
+                  <input
+                    type="file"
+                    name="selectFile"
+                    id="coverBanner"
+                    aria-label="cover banner"
+                    className="selectFile"
+                  />
+                </label>
 
-                  <MyFormItem name="biography" label="Biography">
-                    <TextArea placeholder="Type here" />
-                  </MyFormItem>
+                <label htmlFor="image">Edit Profile Picture</label>
+                <div className="editProfilePicture">
+                  <img src={userImg} alt="edit your profile avatar" />
+                  <div className="btnWrapper">
+                    <button className="btn">Remove</button>
+                    <button className="btn">Change Picture</button>
+                    <button className="btn">Crop Picture</button>
+                  </div>
+                </div>
 
-                  <MyFormItem name="whyChooseMe" label="Why Choose Me?">
-                    <TextArea placeholder="Type here" />
-                  </MyFormItem>
-                </MyFormItemGroup>
+                <label htmlFor="country_or_state">
+                  Country or State & City
+                </label>
+                <Input
+                  aria-label="country_or_state"
+                  placeholder="Type here"
+                  id="country_or_state"
+                />
+
+                <label htmlFor="timeZone">Time Zone</label>
+                <Input
+                  aria-label="timeZone"
+                  placeholder="Type here"
+                  id="timeZone"
+                />
+
+                <label htmlFor="biography">Biography</label>
+                <textarea
+                  placeholder="Type here"
+                  className="textArea"
+                  id="biography"
+                ></textarea>
+
+                <label htmlFor="whyChooseMe">Why Choose Me?</label>
+                <textarea
+                  placeholder="Type here"
+                  className="textArea"
+                  id="whyChooseMe"
+                ></textarea>
               </Form>
             </div>
           </div>
