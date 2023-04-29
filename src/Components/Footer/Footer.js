c95257f05a4ef54ad8b3011e9eb047b802f935c9
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-04-29 10:32:22 +0100

    style: updated home page styling

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import footerLogo from "../../images/footerLogo.png";
+import footerLogo2 from "../../images/spelogo-white.png";
 import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import "./Footer.css";
 
@@ -16,44 +17,50 @@ function Footer() {
         </Link>
       </div>
 
-      <form action="#" method="POST" className="d-flex bg-white footerForm">
-        <h5 className="news my-auto">Subscribe to our newsletter</h5>
-        <input
-          type="text"
-          placeholder="Enter your email"
-          className="footerInput"
-          name="email"
-        />
-        <button type="submit" className="btn footBtn my-auto">
-          Submit
-        </button>
-      </form>
+      <div className="px-3">
+        <form action="#" method="POST" className="bg-white footerForm px-3">
+          <h5 className="news my-auto">Subscribe to our newsletter</h5>
+          <div className="d-flex gap-3">
+            <div className="footerInCont">
+              <input
+                type="text"
+                placeholder="Enter your email"
+                className="footerInput"
+                name="email"
+              />
+            </div>
+            <button type="submit" className="btn footBtn my-auto">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
 
       <div className="footerNav">
-        <div className="col-md-4 text-start">
+        <div className="col-12 col-md-4 text-start">
           <h6 className="fw-bold text-white">NAVIGATION</h6>
           <ul className="list-unstyled">
-            <li className="mb-1">
+            <li className="mb-2">
               <a href={"/"} className="text-decoration-none text-white">
                 Home
               </a>
             </li>
-            <li className="mb-1">
+            <li className="mb-2">
               <a href={"/"} className="text-decoration-none text-white">
                 About
               </a>
             </li>
-            <li className="mb-1">
+            <li className="mb-2">
               <a href={"/"} className="text-decoration-none text-white">
                 Explore
               </a>
             </li>
-            <li className="mb-1">
+            <li className="mb-2">
               <a href={"/"} className="text-decoration-none text-white">
                 Blog
               </a>
             </li>
-            <li className="mb-1">
+            <li className="mb-2">
               <a href={"/"} className="text-decoration-none text-white">
                 Contact
               </a>
@@ -65,10 +72,10 @@ function Footer() {
             </li>
           </ul>
         </div>
-        <div className="col-md-4 text-start">
+        <div className="col-12 col-md-4 text-start">
           <h6 className="fw-bold text-white">LEGAL</h6>
           <ul className="list-unstyled">
-            <li className="mb-1">
+            <li className="mb-2">
               <a href={"/"} className="text-decoration-none text-white">
                 Privacy & Policy
               </a>
@@ -80,36 +87,37 @@ function Footer() {
             </li>
           </ul>
         </div>
-        <div className="col-md-4 text-start">
+
+        <div className="col-12 col-md-4 text-start">
           <h6 className="fw-bold text-white">TALK TO US</h6>
           <ul className="list-unstyled">
-            <li className="mb-1">
+            <li className="mb-2">
               <a href={"/"} className="text-decoration-none text-white">
                 support@spefind.com{" "}
               </a>
             </li>
-            <li className="mb-1">
+            <li className="mb-2">
               <a href={"/"} className="text-decoration-none text-white">
                 +234 1234567891{" "}
               </a>
             </li>
-            <li className="mb-1">
+            <li className="mb-2">
               <a href={"/"} className="text-decoration-none text-white">
                 Contact Us
               </a>
             </li>
-            <li className="mb-1">
+            <li className="mb-2">
               <a href={"/"} className="text-decoration-none text-white">
                 Facebook{" "}
               </a>
             </li>
-            <li className="mb-1">
+            <li className="mb-2">
               <a href={"/"} className="text-decoration-none text-white">
                 {" "}
                 Linkedin
               </a>
             </li>
-            <li className="mb-1">
+            <li className="mb-2">
               <a href={"/"} className="text-decoration-none text-white">
                 Twitter{" "}
               </a>
@@ -118,14 +126,16 @@ function Footer() {
         </div>
       </div>
 
-      <div className="copySection">
-        <img src={footerLogo} alt="logo" className="footerLogo" />
-        <div className="mt-md-5">
+      <div className="copySection mt-5">
+        <div>
+          <img src={footerLogo2} alt="logo" className="footerLogo" />
+        </div>
+        <div className="mt-3 mt-md-0">
           <p className="text-white copyRight">
             &copy; {new Date().getFullYear()} Spefind. All right reserved.
           </p>
         </div>
-        <div className="socialMedia mt-4">
+        <div className="socialMedia mt-3 mt-md-0">
           <div className="icon">
             <FaFacebookF className="social" />
           </div>
