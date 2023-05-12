b800ae643cf89d3133efa798c9b98351e59bc565
Sammydepoj <samueladepoju86@yahoo.com>
2023-05-12 13:49:39 +0100

    fix: code refactoring for dashboard page implemented

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,126 +1,126 @@
-// import React, { useContext, useEffect, useState, createContext } from 'react'
+import React, { useContext, useEffect, useState, createContext } from 'react'
 
-// import {auth} from '../firebase/firebase'
+import {auth} from '../firebase/firebase'
 
-// import {
-//   createUserWithEmailAndPassword,
-//   signInWithEmailAndPassword,
-//   signOut,
-//   onAuthStateChanged,
-//   GoogleAuthProvider,
-//   signInWithPopup
-// } from "firebase/auth"
-
-
-// const AuthContext = createContext()
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup
+} from "firebase/auth"
 
 
+const AuthContext = createContext()
 
 
-// export default function AuthProvider({children}) {
-//   const [currentUser, setCurrentUser] = useState(JSON.parse( localStorage.getItem( 'user' ) ) || null);
-//   const [loading, setLoading] = useState(true);
-//   // Signs up on firebase with email and password.
-//   function signup(email, password) {
-//     return createUserWithEmailAndPassword(auth, email, password);
-//   }
 
 
-//   //user login func
-//   function logIn( email, password ) {
-//             return signInWithEmailAndPassword(auth, email, password )
-//         }
+export default function AuthProvider({children}) {
+  const [currentUser, setCurrentUser] = useState(JSON.parse( localStorage.getItem( 'user' ) ) || null);
+  const [loading, setLoading] = useState(true);
+  // Signs up on firebase with email and password.
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
 
 
-//         //user logout
-//         function logOut() {
-//             return signOut(auth);
-//         }
-
-//         //google authentication
-//   async function googleSignIn() {
-//     const googleAuthProvider = new GoogleAuthProvider();
-//     return signInWithPopup( auth, googleAuthProvider )
-//       .then( result => {
-//         /*Google access token*/
-//         const credential = GoogleAuthProvider.credentialFromResult( result );
-//         const token = credential.accessToken;
-//         const user = result.user;
-//       } ).catch( error => {
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
-//         const email = error.customDate.email;
-//         return null
-//       } );
-//   }
+  //user login func
+  function logIn( email, password ) {
+            return signInWithEmailAndPassword(auth, email, password )
+        }
 
 
-//   // Sets the user to the object and we can keep track if logged in or not.
-//   // useEffect() lets react know to run after render
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged( auth, (user) => {
-//       setCurrentUser(user);
-//       setLoading(false);
-//       localStorage.setItem('user', JSON.stringify(user))
-//     } );
+        //user logout
+        function logOut() {
+            return signOut(auth);
+        }
+
+        //google authentication
+  async function googleSignIn() {
+    const googleAuthProvider = new GoogleAuthProvider();
+    return signInWithPopup( auth, googleAuthProvider )
+      .then( result => {
+        /*Google access token*/
+        const credential = GoogleAuthProvider.credentialFromResult( result );
+        const token = credential.accessToken;
+        const user = result.user;
+      } ).catch( error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customDate.email;
+        return null
+      } );
+  }
+
+
+  // Sets the user to the object and we can keep track if logged in or not.
+  // useEffect() lets react know to run after render
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged( auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+      localStorage.setItem('user', JSON.stringify(user))
+    } );
     
-//     return () => {
-//       unsubscribe();
-//   }
-//   }, [currentUser]);
-//   // Export the value for other functions to call it.
-//   const value = {
-//     currentUser,
-//     signup,
-//     logIn,
-//     logOut,
-//     googleSignIn
-//   };
-//   return (
-//     <AuthContext.Provider value={value}>
-//       {!loading && children}
-//     </AuthContext.Provider>
-//   );
-// }
-
-// // export function useAuth();
-// // return useContext();
-// export const useAuth = () => {
-//   return useContext(AuthContext);
-// };
-
-import React, { useState } from "react";
-
-// eslint-disable-next-line no-undef
-const AuthContext = React.createContext({
-  token: "",
-  isLoggedIn: false,
-  login: (token) => {},
-  logout: () => {},
-});
-
-export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
-
-  const userIsLoggedIn = !!token;
-
-  const loginHandler = (token) => {
-    setToken(token);
-  };
-  const logoutHandler = () => {
-    setToken(null);
-  };
-
-  const contextValue = {
-    token: token,
-    isLoggedIn: userIsLoggedIn,
-    login: loginHandler,
-    logout: logoutHandler,
+    return () => {
+      unsubscribe();
+  }
+  }, [currentUser]);
+  // Export the value for other functions to call it.
+  const value = {
+    currentUser,
+    signup,
+    logIn,
+    logOut,
+    googleSignIn
   };
   return (
-    <AuthContext.Provider value={contextValue}>
-      {props.children}
+    <AuthContext.Provider value={value}>
+      {!loading && children}
     </AuthContext.Provider>
   );
+}
+
+// export function useAuth();
+// return useContext();
+export const useAuth = () => {
+  return useContext(AuthContext);
 };
-export default AuthContext;
+
+// import React, { useState } from "react";
+
+// // eslint-disable-next-line no-undef
+// const AuthContext = React.createContext({
+//   token: "",
+//   isLoggedIn: false,
+//   login: (token) => {},
+//   logout: () => {},
+// });
+
+// export const AuthContextProvider = (props) => {
+//   const [token, setToken] = useState(null);
+
+//   const userIsLoggedIn = !!token;
+
+//   const loginHandler = (token) => {
+//     setToken(token);
+//   };
+//   const logoutHandler = () => {
+//     setToken(null);
+//   };
+
+//   const contextValue = {
+//     token: token,
+//     isLoggedIn: userIsLoggedIn,
+//     login: loginHandler,
+//     logout: logoutHandler,
+//   };
+//   return (
+//     <AuthContext.Provider value={contextValue}>
+//       {props.children}
+//     </AuthContext.Provider>
+//   );
+// };
+// export default AuthContext;
---
+++ src/pages/Dashboard/components/Upgrade/Upgrade.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import "./Upgrade.css";
+
+const Upgrade = () => {
+  const [isMonthlyPlan, setIsMonthlyPlan] = useState(true);
+
+  return (
+    <div className="upgradeContainer">
+      <div className="d-flex justify-content-center gap-3 align-items-center">
+        <button
+          className={isMonthlyPlan ? "btn-orange" : "btn-orange-line"}
+          onClick={() => setIsMonthlyPlan(true)}
+        >
+          Monthly Plan
+        </button>
+        <button
+          className={isMonthlyPlan ? "btn-orange-line" : "btn-orange"}
+          onClick={() => setIsMonthlyPlan(false)}
+        >
+          Yearly Plan
+        </button>
+      </div>
+
+      <div className="upgradeBox mx-auto my-5 w-100 p-5">
+        <span className="upgradeType">Free</span>
+        <p className="text-center fw-bold fs-5 mb-2">
+          No charges applied <br /> ₦0.00
+        </p>
+
+        <div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Publish your communicator profile</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Add 1 photo or slide</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Limited visibility</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>A few free speaking opportunities</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Event organizers can contact you</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Speaking tips and helpful articles</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>No commission or transaction fee</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="upgradeBox mx-auto my-5 w-100 p-5">
+        <span className="upgradeType">Paid</span>
+        <p className="text-center fw-bold fs-5 mb-2">
+          Charges Applied <br /> {isMonthlyPlan ? "₦5,000 /m" : "₦50,000 /yr"}
+        </p>
+
+        <div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Publish your communicator profile</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Unlimited photos, slides, and videos</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Unlimited visibility</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>All free speaking opportunities</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Exclusive content and Webinars</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Event organizers can contact you</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Recommended speaking opportunities</p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Get featured in search </p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Detailed profile statistics </p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Share presentations publicly </p>
+          </div>
+          <div className="upgradeOption d-flex gap-3 align-items-center mb-3">
+            <span></span> <p>Add workshop or training agendas. </p>
+          </div>
+        </div>
+
+        <div>
+          <button disabled className="btn-disabled mx-auto d-block mt-5 mb-3">
+            Free 30-Day Trial
+          </button>
+          <button className="btn-green mx-auto d-block">Select</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Upgrade;
