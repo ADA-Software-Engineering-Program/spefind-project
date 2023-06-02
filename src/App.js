180225bfa5a611ea6150b947f4a9ca680d1cec23
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-06-03 00:02:02 +0100

    fix: configuring eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import ProtectedRoutes from './Router/Protected'
-import Home from './pages/Home/Home'
-import About from '../src/pages/About/About'
-import Explore from './pages/Explore/Explore'
-import Blog from './pages/Blog/Blog'
-import Contact from './pages/Contact/Contact'
-import SignIn from '../src/Components/SignIn/SignIn'
-import SignUp from '../src/Components/SignUp/SignUp'
-import Register from './pages/Register/Register'
-import EventRegister from './pages/Register/EventRegister'
-import CreateProfile from './pages/CreateProfile/CreateProfile'
-import SpeakerProfile from './pages/SpeakerProfile/SpeakerProfile'
-import Bookme from './pages/BookMe/Bookme.jsx'
-import FAQ from './pages/FAQ/FAQ'
-import Oops from './pages/Oops/Oops'
-import Congratulation from './pages/Congratulation/Congratulation'
-import Congratulation2 from './pages/Congratulation/Congratulation2'
-import Chat from './pages/Chat/Chat'
-import Dashboard from './pages/Dashboard/Dashboard'
+import "./App.css"
+import { Route, Routes } from "react-router-dom"
+import ProtectedRoutes from "./Router/Protected"
+import Home from "./pages/Home/Home"
+import About from "../src/pages/About/About"
+import Explore from "./pages/Explore/Explore"
+import Blog from "./pages/Blog/Blog"
+import Contact from "./pages/Contact/Contact"
+import SignIn from "../src/Components/SignIn/SignIn"
+import SignUp from "../src/Components/SignUp/SignUp"
+import Register from "./pages/Register/Register"
+import EventRegister from "./pages/Register/EventRegister"
+import CreateProfile from "./pages/CreateProfile/CreateProfile"
+import SpeakerProfile from "./pages/SpeakerProfile/SpeakerProfile"
+import Bookme from "./pages/BookMe/Bookme.jsx"
+import FAQ from "./pages/FAQ/FAQ"
+import Oops from "./pages/Oops/Oops"
+import Congratulation from "./pages/Congratulation/Congratulation"
+import Congratulation2 from "./pages/Congratulation/Congratulation2"
+import Chat from "./pages/Chat/Chat"
+import Dashboard from "./pages/Dashboard/Dashboard"
 
 function App() {
   function isLoggedIn() {
-    const isAuthenticated = !!sessionStorage.getItem('token')
+    const isAuthenticated = !!sessionStorage.getItem("token")
     return isAuthenticated
   }
 
