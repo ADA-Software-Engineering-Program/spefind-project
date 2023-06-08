797e392a4571848cc35181f34f64ff69a6281093
Sammydepoj <samueladepoju86@yahoo.com>
2023-06-08 15:18:52 +0100

    fix:availableTo states concatenation to other states fixes in progress

--- src/pages/CreateProfile/CreateProfile.js
+++ src/pages/CreateProfile/CreateProfile.js
@@ -100,7 +100,6 @@ const CreateProfile = () => {
         firstName: userData?.user?.firstName,
         lastName: userData?.user?.lastName
       }))
-      // getEventTypeData,getAvailableToData,getPriceData,getSpeakerFieldOption,getUserData
     } catch (error) {
       console.log(error)
     }
@@ -109,6 +108,8 @@ const CreateProfile = () => {
   const formik = useFormik({
     initialValues: {
       // personal
+      // firstName: "",
+      // lastName: "",
       gender: "",
       country: "",
       city: "",
@@ -118,6 +119,7 @@ const CreateProfile = () => {
       location: "",
       numberOfAttendees: "",
       field: "",
+      // eventPictures: "",
 
       // niche
       speakerField: "",
@@ -196,41 +198,41 @@ const CreateProfile = () => {
         isVisible: formik.values.isVisible
       }
       console.log(transformedData)
-      // const token = sessionStorage.getItem("token")
-      // const saveUserData = await fetch(`${API_LINK}/api/profile/setup`, {
-      //   method: "PUT",
-      //   body: JSON.stringify(transformedData),
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //     Authorization: `Bearer ${token}`,
-      //     redirect: "follow"
-      //   }
-      // })
-      // const data = await saveUserData.json()
-      // console.log(data)
+      const token = sessionStorage.getItem("token")
+      const saveUserData = await fetch(`${API_LINK}/api/profile/setup`, {
+        method: "PUT",
+        body: JSON.stringify(transformedData),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          redirect: "follow"
+        }
+      })
+      const data = await saveUserData.json()
+      console.log(data)
 
-      // if (saveUserData.ok) {
-      //   toast.success(`${data.message}`, {
-      //     duration: 4000,
-      //     position: "top-center",
+      if (saveUserData.ok) {
+        toast.success(`${data.message}`, {
+          duration: 4000,
+          position: "top-center",
 
-      //     // Styling
-      //     style: { fontSize: "13px", border: "2px solid green" },
-      //     className: ""
-      //   })
-      //   sessionStorage.setItem("userId", data.user._id)
-      //   // navigate("/dashboard")
-      //   window.scrollTo(0, 0)
-      // }
+          // Styling
+          style: { fontSize: "13px", border: "2px solid green" },
+          className: ""
+        })
+        sessionStorage.setItem("userId", data.user._id)
+        // navigate("/dashboard")
+        window.scrollTo(0, 0)
+      }
 
-      // if (!saveUserData) {
-      //   setError("Please cross check your details and try again")
-      //   throw new Error(error)
-      // }
-      // if (!saveUserData.ok) {
-      //   setError("Your session might have expired, please try to log in again!")
-      //   throw new Error(error)
-      // }
+      if (!saveUserData) {
+        setError("Please cross check your details and try again")
+        throw new Error(error)
+      }
+      if (!saveUserData.ok) {
+        setError("Your session might have expired, please try to log in again!")
+        throw new Error(error)
+      }
     } catch (error) {
       console.log(error)
       toast.error(`${error}`, {
